7227cf24cfd3a3832c71dc8dadc3d5df76c8f062
PIC-WILLIANS <willians.segundo@virtex.com.br>
2025-10-03 13:05:18 -0300

    Add comentario

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import hashlib
 texto = "Minha mensagem segura"
 dados_em_bytes = texto.encode('utf-8')
 
-meu_hash = hashlib.sha256(dados_em_bytes).hexdigest()
+
+# Retorna o hash)como uma string de dígitos hexadecimais.
+meu_hash = hashlib.sha256(dados_em_bytes).hexdigest() 
+
 
 print(f"Meu texto normal: {texto}")
 print(f"Meu hash-sha256: {meu_hash}")
